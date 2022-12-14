ae36b5de7bb1394246cbaacd839f489f115a4a45
suprakron <suprakron15@gmail.com>
2022-12-14 13:03:28 +0700

    first commit

---
+++ config/connect.php
@@ -0,0 +1,17 @@
+<?php
+	
+	$servername = "localhost";
+	$dbname = "web_board";
+	$username = "root";
+	$password = "";
+
+	try {
+
+		$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+		$conn->exec("SET NAMES UTF8");
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+	} catch (Exception $e) {
+		echo $e->getMessage();
+	}
+?>
